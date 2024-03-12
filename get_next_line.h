802a4d574ca7bb49777ed7a1e7502b33112e62a3
pbumi <pbumidang@gmail.com>
2024-03-12 16:52:21 +0200

    norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pbumidan <pbumidan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:47:23 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/02/25 17:08:35 by pbumidan         ###   ########.fr       */
+/*   Updated: 2024/03/12 16:50:22 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
-
 
 #endif
