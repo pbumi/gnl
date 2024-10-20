622b49206732be77737bd43d8f0c4a4339c3ea6b
pbumihome <pbumidan@DESKTOP-6RNAT92>
2024-10-20 16:35:16 +0300

    getendline function created

--- get_all_lines.c
+++ get_all_lines.c
@@ -6,7 +6,7 @@
 /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 17:05:50 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/10/15 17:49:49 by pbumidan         ###   ########.fr       */
+/*   Updated: 2024/10/20 16:32:07 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,33 @@
 char	*get_all_lines(int fd)
 {
 	static char	*source;
+	int			bytes_read;
+
+	if (!source)
+	{
+		source = (char *)malloc(sizeof(char));
+		if (!source)
+			return (NULL);
+		source[0] = '\0';
+	}
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
+	{
+		return (ft_free(&source));
+	}
+	bytes_read = ft_read_source(fd, &source);
+	while (bytes_read > 0)
+	{
+		bytes_read = ft_read_source(fd, &source);
+	}
+	if (bytes_read < 0 || *source == '\0')
+	{
+		return (ft_free(&source));
+	}
+	return (source);
+}
+
+char	*get_end_lines(int fd, char	*source)
+{
 	int			bytes_read;
 
 	if (!source)
