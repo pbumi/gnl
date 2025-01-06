4ee103a235a0daa3bd3480d70246e09d4d635ec7
Paul Bumidang <pbumidan@c3r3p6.hive.fi>
2025-01-06 18:14:09 +0200

    gnl fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,132 +6,229 @@
 /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:47:33 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/10/20 15:02:32 by pbumidan         ###   ########.fr       */
+/*   Updated: 2025/01/06 18:13:26 by pbumidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-char *ft_free(char **str)
-{
-    if (str && *str)
-    {
-        free(*str);  // Free memory pointed by *str
-        *str = NULL; // Set the pointer to NULL to avoid dangling pointers
-    }
-    return (NULL); // Return NULL to set the calling pointer to NULL
-}
-// char	*ft_free(char **str)
+// char *ft_free(char **str)
 // {
-// 	free (*str);
-// 	*str = NULL;
-// 	return (NULL);
+//     if (str && *str)
+//     {
+//         free(*str);  // Free memory pointed by *str
+//         *str = NULL; // Set the pointer to NULL to avoid dangling pointers
+//     }
+//     return (NULL); // Return NULL to set the calling pointer to NULL
+// }
+// // char	*ft_free(char **str)
+// // {
+// // 	free (*str);
+// // 	*str = NULL;
+// // 	return (NULL);
+// // }
+
+// static char *ft_free_all(char **source, char **str)
+// {
+//     if (str && *str)
+//     {
+//         free(*str);  // Free memory pointed by *str
+//         *str = NULL; // Set the pointer to NULL
+//     }
+//     if (source && *source)
+//     {
+//         *source = NULL; // Set source to NULL explicitly
+//     }
+//     return (NULL); // Return NULL to indicate both have been cleared
+// }
+// // static char	*ft_free_all(char **source, char **str)
+// // {
+// // 	free (*str);
+// // 	*source = NULL;
+// // 	return (NULL);
+// // }
+
+// static char	*ft_get_line(char **source)
+// {
+// 	int		x;
+// 	char	*line;
+// 	char	*old_source;
+// 	char	*new_source;
+
+// 	x = 0;
+// 	old_source = *source;
+// 	while (old_source[x] && old_source[x] != '\n')
+// 	{
+// 		x++;
+// 	}
+// 	line = gnl_substr(old_source, 0, x + 1);
+// 	if (!line)
+// 	{
+// 		return (ft_free_all(source, &old_source));
+// 	}
+// 	new_source = gnl_substr(old_source, x + 1, gnl_strlen(old_source));
+// 	if (!new_source)
+// 	{
+// 		free (line);
+// 		return (ft_free_all(source, &old_source));
+// 	}
+// 	free (old_source);
+// 	*source = new_source;
+// 	return (line);
 // }
 
-static char *ft_free_all(char **source, char **str)
-{
-    if (str && *str)
-    {
-        free(*str);  // Free memory pointed by *str
-        *str = NULL; // Set the pointer to NULL
-    }
-    if (source && *source)
-    {
-        *source = NULL; // Set source to NULL explicitly
-    }
-    return (NULL); // Return NULL to indicate both have been cleared
-}
-// static char	*ft_free_all(char **source, char **str)
+// int	ft_read_source(int fd, char **source)
 // {
-// 	free (*str);
-// 	*source = NULL;
-// 	return (NULL);
+// 	char	*buffer;
+// 	char	*temp;
+// 	int		bytes_read;
+
+// 	temp = *source;
+// 	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+// 	if (!buffer)
+// 		return (-1);
+// 	bytes_read = read(fd, buffer, BUFFER_SIZE);
+// 	if (bytes_read < 0)
+// 	{
+// 		free (buffer);
+// 		return (-1);
+// 	}
+// 	buffer[bytes_read] = '\0';
+// 	*source = gnl_strjoin(temp, buffer);
+// 	if (!*source)
+// 	{
+// 		free (temp);
+// 		free (buffer);
+// 		return (-1);
+// 	}
+// 	free(temp);
+// 	free(buffer);
+// 	return (bytes_read);
 // }
 
-static char	*ft_get_line(char **source)
-{
-	int		x;
-	char	*line;
-	char	*old_source;
-	char	*new_source;
+// char	*get_next_line(int fd)
+// {
+// 	static char	*source = NULL;
+// 	int			bytes_read;
 
-	x = 0;
-	old_source = *source;
-	while (old_source[x] && old_source[x] != '\n')
-	{
-		x++;
-	}
-	line = gnl_substr(old_source, 0, x + 1);
-	if (!line)
-	{
-		return (ft_free_all(source, &old_source));
-	}
-	new_source = gnl_substr(old_source, x + 1, gnl_strlen(old_source));
-	if (!new_source)
-	{
-		free (line);
-		return (ft_free_all(source, &old_source));
-	}
-	free (old_source);
-	*source = new_source;
-	return (line);
+// 	if (fd < 0) // Special case to manually free static memory
+// 	{
+// 		ft_free(&source);  // Free and set to NULL
+// 		return (NULL);
+// 	}
+// 	if (!source)
+// 	{
+// 		source = (char *)malloc(sizeof(char));
+// 		if (!source)
+// 			return (NULL);
+// 		source[0] = '\0';
+// 	}
+// 	if (BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
+// 		return (ft_free(&source));
+// 	bytes_read = ft_read_source(fd, &source);
+// 	while (gnl_strchr(source, '\n') == 0 && bytes_read > 0)
+// 		bytes_read = ft_read_source(fd, &source);
+// 	if (bytes_read < 0 || *source == '\0')
+// 	{
+// 		return (ft_free(&source));
+// 	}
+// 	return (ft_get_line(&source));
+// }
+
+
+
+void *ft_free(void **ptr)
+{
+    if (ptr && *ptr)
+    {
+        free(*ptr);
+        *ptr = NULL;
+    }
+    return (NULL);
 }
 
-int	ft_read_source(int fd, char **source)
+static char *ft_get_line(char **source)
 {
-	char	*buffer;
-	char	*temp;
-	int		bytes_read;
+    int x = 0;
+    char *line;
+    char *old_source = *source;
+    
+    while (old_source[x] && old_source[x] != '\n')
+        x++;
 
-	temp = *source;
-	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (-1);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
-	{
-		free (buffer);
-		return (-1);
-	}
-	buffer[bytes_read] = '\0';
-	*source = gnl_strjoin(temp, buffer);
-	if (!*source)
-	{
-		free (temp);
-		free (buffer);
-		return (-1);
-	}
-	free(temp);
-	free(buffer);
-	return (bytes_read);
+    line = gnl_substr(old_source, 0, x + 1);
+    if (!line)
+        return (ft_free(&old_source));
+
+    char *new_source = gnl_substr(old_source, x + 1, gnl_strlen(old_source));
+    if (!new_source)
+    {
+        free(line);
+        return (ft_free(&old_source));
+    }
+
+    free(old_source);
+    *source = new_source;
+    return (line);
 }
 
-char	*get_next_line(int fd)
+int ft_read_source(int fd, char **source)
 {
-	static char	*source = NULL;
-	int			bytes_read;
+    char *buffer = malloc(BUFFER_SIZE + 1);
+    if (!buffer)
+        return (-1);
 
-	if (fd < 0) // Special case to manually free static memory
-	{
-		ft_free(&source);  // Free and set to NULL
-		return (NULL);
-	}
-	if (!source)
-	{
-		source = (char *)malloc(sizeof(char));
-		if (!source)
-			return (NULL);
-		source[0] = '\0';
-	}
-	if (BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
-		return (ft_free(&source));
-	bytes_read = ft_read_source(fd, &source);
-	while (gnl_strchr(source, '\n') == 0 && bytes_read > 0)
-		bytes_read = ft_read_source(fd, &source);
-	if (bytes_read < 0 || *source == '\0')
-	{
-		return (ft_free(&source));
-	}
-	return (ft_get_line(&source));
+    int bytes_read = read(fd, buffer, BUFFER_SIZE);
+    if (bytes_read < 0)
+    {
+        free(buffer);
+        return (-1);
+    }
+
+    buffer[bytes_read] = '\0';
+    char *temp = *source;
+    *source = gnl_strjoin(temp, buffer);
+
+    if (!*source)
+    {
+        free(temp);
+        free(buffer);
+        return (-1);
+    }
+
+    free(temp);
+    free(buffer);
+    return (bytes_read);
+}
+
+char *get_next_line(int fd)
+{
+    static char *source = NULL;
+
+    if (fd < 0) 
+    {
+        ft_free(&source);
+        return (NULL);
+    }
+
+    if (!source)
+    {
+        source = malloc(1);
+        if (!source)
+            return (NULL);
+        source[0] = '\0';
+    }
+
+    if (BUFFER_SIZE <= 0)
+        return (ft_free(&source));
+
+    int bytes_read = ft_read_source(fd, &source);
+    while (gnl_strchr(source, '\n') == NULL && bytes_read > 0)
+        bytes_read = ft_read_source(fd, &source);
+
+    if (bytes_read < 0 || *source == '\0')
+        return (ft_free(&source));
+
+    return (ft_get_line(&source));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,99 +1,164 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/15 14:47:47 by pbumidan          #+#    #+#             */
-/*   Updated: 2025/01/06 18:08:14 by pbumidan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   get_next_line_utils.c                              :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/12/15 14:47:47 by pbumidan          #+#    #+#             */
+// /*   Updated: 2025/01/06 18:08:14 by pbumidan         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+// #include "get_next_line.h"
+
+// size_t	gnl_strlen(const char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i] != 0)
+// 		i++;
+// 	return (i);
+// }
+
+// char	*gnl_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char	*substr;
+// 	size_t	slen;
+// 	size_t	i;
+
+// 	if (len == 0 || s == NULL)
+// 		return (NULL);
+// 	slen = gnl_strlen(s);
+// 	if (start >= slen)
+// 		len = 0;
+// 	if (len > (slen - start))
+// 		len = (slen - start);
+// 	substr = calloc(len + 1, sizeof(char)); //malloc((sizeof(char)) * (len + 1));
+// 	if (!substr)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		substr[i] = s[start];
+// 		i++;
+// 		start++;
+// 	}
+// 	substr[i] = '\0';
+// 	return (substr);
+// }
+
+// char	*gnl_strchr(const char *s, int c)
+// {
+// 	size_t	i;
+// 	size_t	slen;
+
+// 	i = 0;
+// 	if (!s)
+// 		return (0);
+// 	slen = gnl_strlen(s);
+// 	while (i < slen)
+// 	{
+// 		if (s[i] == (char)c)
+// 		{
+// 			return ((char *)s + i);
+// 		}
+// 			//return (&((char *)s)[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+// char	*gnl_strjoin(char const *s1, char const *s2)
+// {
+// 	char	*str;
+// 	size_t	a;
+// 	size_t	b;
+
+// 	if (s1 == NULL || s2 == NULL)
+// 		return (NULL);
+// 	str = ((char *)malloc(sizeof(char) * (gnl_strlen(s1) + gnl_strlen(s2) + 1)));
+// 	if (str == NULL)
+// 		return (NULL);
+// 	a = 0;
+// 	while (s1[a])
+// 	{
+// 		str[a] = s1[a];
+// 		a++;
+// 	}
+// 	b = 0;
+// 	while (s2[b])
+// 	{
+// 		str[a] = s2[b];
+// 		a++;
+// 		b++;
+// 	}
+// 	str[a] = '\0';
+// 	return (str);
+// }
 
 #include "get_next_line.h"
 
-size_t	gnl_strlen(const char *s)
+size_t gnl_strlen(const char *s)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] != 0)
-		i++;
-	return (i);
+    size_t i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
 }
 
-char	*gnl_substr(char const *s, unsigned int start, size_t len)
+char *gnl_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
-	size_t	slen;
-	size_t	i;
+    if (len == 0 || s == NULL)
+        return (NULL);
 
-	if (len == 0 || s == NULL)
-		return (NULL);
-	slen = gnl_strlen(s);
-	if (start >= slen)
-		len = 0;
-	if (len > (slen - start))
-		len = (slen - start);
-	substr = calloc(len + 1, sizeof(char)); //malloc((sizeof(char)) * (len + 1));
-	if (!substr)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		substr[i] = s[start];
-		i++;
-		start++;
-	}
-	substr[i] = '\0';
-	return (substr);
+    size_t slen = gnl_strlen(s);
+    if (start >= slen)
+        len = 0;
+    if (len > (slen - start))
+        len = (slen - start);
+
+    char *substr = malloc(len + 1);
+    if (!substr)
+        return (NULL);
+
+    memcpy(substr, s + start, len);  // Use memcpy for efficiency
+    substr[len] = '\0';
+    return (substr);
 }
 
-char	*gnl_strchr(const char *s, int c)
+char *gnl_strchr(const char *s, int c)
 {
-	size_t	i;
-	size_t	slen;
+    size_t i = 0;
+    size_t slen = gnl_strlen(s);
 
-	i = 0;
-	if (!s)
-		return (0);
-	slen = gnl_strlen(s);
-	while (i < slen)
-	{
-		if (s[i] == (char)c)
-		{
-			return ((char *)s + i);
-		}
-			//return (&((char *)s)[i]);
-		i++;
-	}
-	return (0);
+    if (!s)
+        return (NULL);
+
+    while (i < slen)
+    {
+        if (s[i] == (char)c)
+            return ((char *)s + i);
+        i++;
+    }
+    return (NULL);
 }
 
-char	*gnl_strjoin(char const *s1, char const *s2)
+char *gnl_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	a;
-	size_t	b;
+    if (!s1 || !s2)
+        return (NULL);
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	str = ((char *)malloc(sizeof(char) * (gnl_strlen(s1) + gnl_strlen(s2) + 1)));
-	if (str == NULL)
-		return (NULL);
-	a = 0;
-	while (s1[a])
-	{
-		str[a] = s1[a];
-		a++;
-	}
-	b = 0;
-	while (s2[b])
-	{
-		str[a] = s2[b];
-		a++;
-		b++;
-	}
-	str[a] = '\0';
-	return (str);
+    size_t len1 = gnl_strlen(s1);
+    size_t len2 = gnl_strlen(s2);
+
+    char *str = malloc(len1 + len2 + 1);
+    if (!str)
+        return (NULL);
+
+    memcpy(str, s1, len1);
+    memcpy(str + len1, s2, len2);
+    str[len1 + len2] = '\0';
+    return (str);
 }
