cb0f577155ea86d6c67e9be9f6b4405726a9423c
Paul Bumidang <pbumidan@c3r3p6.hive.fi>
2025-01-06 18:30:57 +0200

    gal commented out

--- get_all_lines.c
+++ get_all_lines.c
@@ -1,68 +1,68 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_all_lines.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/25 17:05:50 by pbumidan          #+#    #+#             */
-/*   Updated: 2024/10/20 16:42:09 by pbumidan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   get_all_lines.c                                    :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: pbumidan <pbumidan@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2024/02/25 17:05:50 by pbumidan          #+#    #+#             */
+// /*   Updated: 2024/10/20 16:42:09 by pbumidan         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "get_next_line.h"
+// #include "get_next_line.h"
 
-char	*get_all_lines(int fd)
-{
-	static char	*source;
-	int			bytes_read;
+// char	*get_all_lines(int fd)
+// {
+// 	static char	*source;
+// 	int			bytes_read;
 
-	if (!source)
-	{
-		source = (char *)malloc(sizeof(char));
-		if (!source)
-			return (NULL);
-		source[0] = '\0';
-	}
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
-	{
-		return (ft_free(&source));
-	}
-	bytes_read = ft_read_source(fd, &source);
-	while (bytes_read > 0)
-	{
-		bytes_read = ft_read_source(fd, &source);
-	}
-	if (bytes_read < 0 || *source == '\0')
-	{
-		return (ft_free(&source));
-	}
-	return (source);
-}
+// 	if (!source)
+// 	{
+// 		source = (char *)malloc(sizeof(char));
+// 		if (!source)
+// 			return (NULL);
+// 		source[0] = '\0';
+// 	}
+// 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
+// 	{
+// 		return (ft_free(&source));
+// 	}
+// 	bytes_read = ft_read_source(fd, &source);
+// 	while (bytes_read > 0)
+// 	{
+// 		bytes_read = ft_read_source(fd, &source);
+// 	}
+// 	if (bytes_read < 0 || *source == '\0')
+// 	{
+// 		return (ft_free(&source));
+// 	}
+// 	return (source);
+// }
 
-char	*get_end_line(int fd, char	*source)
-{
-	int			bytes_read;
+// char	*get_end_line(int fd, char	*source)
+// {
+// 	int			bytes_read;
 
-	if (!source)
-	{
-		source = (char *)malloc(sizeof(char));
-		if (!source)
-			return (NULL);
-		source[0] = '\0';
-	}
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
-	{
-		return (ft_free(&source));
-	}
-	bytes_read = ft_read_source(fd, &source);
-	while (bytes_read > 0)
-	{
-		bytes_read = ft_read_source(fd, &source);
-	}
-	if (bytes_read < 0 || *source == '\0')
-	{
-		return (ft_free(&source));
-	}
-	return (source);
-}
+// 	if (!source)
+// 	{
+// 		source = (char *)malloc(sizeof(char));
+// 		if (!source)
+// 			return (NULL);
+// 		source[0] = '\0';
+// 	}
+// 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &source, 0) < 0)
+// 	{
+// 		return (ft_free(&source));
+// 	}
+// 	bytes_read = ft_read_source(fd, &source);
+// 	while (bytes_read > 0)
+// 	{
+// 		bytes_read = ft_read_source(fd, &source);
+// 	}
+// 	if (bytes_read < 0 || *source == '\0')
+// 	{
+// 		return (ft_free(&source));
+// 	}
+// 	return (source);
+// }
